56a3d35964acb2f386b9a8b6316b4acedfc4528a
Locullus <bula.mickael@gmail.com>
2022-09-16 19:01:23 +0200

    WIP: fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Constructeur;
 use App\Entity\Voiture;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -13,12 +14,24 @@ class AppFixtures extends Fixture
         $faker = (new \Faker\Factory())::create();
         $faker->addProvider(new \Faker\Provider\Fakecar($faker));
 
-        // liste de constructeurs
+        // liste des constructeurs
+        $constructors = [
+            ['Renault', 'France'],
+            ['Peugeot', 'France'],
+            ['Dacia', 'France'],
+            ['BMW', 'Allemagne'],
+            ['Mercedes', 'Allemagne'],
+            ['Skoda', 'République Tchèque'],
+        ];
+
+        // liste de modèles
         $brands = [
-            'Renault',
-            'Peugeot',
-            'Dacia',
-            'BMW',
+            'Renault' => ['Clio', 'Mégane', 'Scénic'],
+            'Peugeot' => ['308', '3008', '2008'],
+            'Dacia' => ['Sandero', 'Duster'],
+            'BMW' => ['X1', 'X3', 'M4'],
+            'Mercedes' => ['class A', 'class B', 'class C'],
+            'Skoda' => ['Octavia', 'Superb', 'Fabia'],
         ];
 
         // liste des carburants
@@ -38,12 +51,32 @@ class AppFixtures extends Fixture
             return [$brand, $model];
         }
 
-        [$brand, $model] = getConstructorFromArray($brands);
+        // création des constructeurs
+        $fixtureBrands = [];
+        foreach ($constructors as $constructor) {
+            foreach ($constructor as $brand) {
+                $constructeur = new Constructeur();
+                $constructeur->setName($brand[0]);
+                $constructeur->setCountry($brand[1]);
+                $site = strtolower($brand[0]) . '@gmail.com';
+                $constructeur->setSite($site);
+
+                $fixtureBrands[] = $constructeur;
+            }
+        }
 
         // création de 10 véhicules
         for ($i=0; $i <= 10; $i++) {
+            // récupération d'une marque et d'un modèle
+            [$brand, $model] = getConstructorFromArray($brands);
+
+            // il faut récupérer l'objet constructeur qui correspond au nom $brand pour le fournir à l'objet voiture
+            // chercher dans fixtureBrands $constructeur->getName() === $brand;
+
+            // création de l'objet voiture
             $voiture = new Voiture();
             $voiture->setModel($model);
+
             $voiture->setConstructor($brand);
             $voiture->setEnergy($fuels[array_rand($fuels)]);
             $voiture->setSeat(mt_rand(1, 5));
