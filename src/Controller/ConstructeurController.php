84d71c9321b0a01608d06625157b357f1f610e26
Locullus <bula.mickael@gmail.com>
2022-09-18 18:07:52 +0200

    add cache when fetching the list of constructors

--- src/Controller/ConstructeurController.php
+++ src/Controller/ConstructeurController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 /**
  * @Route("/constructeur")
@@ -18,10 +19,13 @@ class ConstructeurController extends AbstractController
     /**
      * @Route("/", name="app_constructeur_index", methods={"GET"})
      */
-    public function index(ConstructeurRepository $constructeurRepository): Response
+    public function index(ConstructeurRepository $constructeurRepository, CacheInterface $cache): Response
     {
+        $constructors = $cache->get('constructors', function() use ($constructeurRepository) {
+            return $constructeurRepository->findAll();
+        });
         return $this->render('constructeur/index.html.twig', [
-            'constructeurs' => $constructeurRepository->findAll(),
+            'constructeurs' => $constructors,
         ]);
     }
 
