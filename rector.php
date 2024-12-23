fbf081f5b91431a3529b6c55c11f129e38e8372f
Locullus <bula.mickael@gmail.com>
2024-12-23 12:32:16 +0100

    Supprime code déprécié
    Remplace FlashBagInterface par SessionInterface dans AppAuthenticator.php

--- rector.php
+++ rector.php
@@ -3,14 +3,15 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\SymfonySetList;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ])
+                    __DIR__ . '/config',
+                    __DIR__ . '/public',
+                    __DIR__ . '/src',
+                    __DIR__ . '/tests',
+                ])
     // uncomment to reach your current PHP version
     ->withPreparedSets(
         deadCode:           true,
@@ -19,4 +20,9 @@ return RectorConfig::configure()
         typeDeclarations:   true,
         earlyReturn:        true,
         symfonyCodeQuality: true
+    )
+    ->withSets([
+                   SymfonySetList::SYMFONY_60,
+               ]
     );
+
